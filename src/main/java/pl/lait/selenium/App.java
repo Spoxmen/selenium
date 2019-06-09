ef72d1db0ec3c6af481d664d0b14352212cf6146
Spoxmen <spoxmen@gmail.com>
2019-06-09 13:18:00 +0200

    Zmienione wielkości zmiennej sleep

--- src/main/java/pl/lait/selenium/App.java
+++ src/main/java/pl/lait/selenium/App.java
@@ -23,9 +23,9 @@ public class App
         driver.findElement(By.linkText("CONTACT")).click();
         sleep(2);
         driver.findElement(By.linkText("SUPPORT")).click();
-        sleep(2);
-        driver.findElement(By.linkText("REGISTER")).click();
         sleep(3);
+        driver.findElement(By.linkText("REGISTER")).click();
+        sleep(4);
         driver.findElement(By.linkText("SIGN-ON")).click();
         
     }
