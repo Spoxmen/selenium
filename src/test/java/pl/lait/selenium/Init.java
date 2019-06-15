17fcbce8b79cfd10184fbcebca2e3138c4ab11c3
Spoxmen <spoxmen@gmail.com>
2019-06-15 15:00:15 +0200

    Dodano obiekty na stronie

--- src/test/java/pl/lait/selenium/Init.java
+++ src/test/java/pl/lait/selenium/Init.java
@@ -10,11 +10,13 @@ public class Init {
 	public static WebDriver getDriver() {
 
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\user1\\chromedriver.exe");
-
-		driver = new ChromeDriver();
-		driver.get("http://newtours.demoaut.com/");
-
-		return driver;
+		if (driver == null) {
+			driver = new ChromeDriver();
+			driver.get("http://newtours.demoaut.com/");
+			return driver;
+		} else {
+			return driver;
+		}
 	}
 
 	public static void sleep(int seconds) {
@@ -29,6 +31,7 @@ public class Init {
 
 	public static void close() {
 		driver.quit();
+		driver = null;
 	}
 
 }
