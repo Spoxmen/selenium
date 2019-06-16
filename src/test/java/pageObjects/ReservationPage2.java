b092063198f044d786fe9110c0e2f7812902f01c
Spoxmen <spoxmen@gmail.com>
2019-06-16 12:05:06 +0200

    Dodano obiekty i metody na stronie rezerwacji 2

--- src/test/java/pageObjects/ReservationPage2.java
+++ src/test/java/pageObjects/ReservationPage2.java
@@ -1,6 +1,8 @@
 package pageObjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import pl.lait.selenium.Init;
@@ -9,11 +11,45 @@ public class ReservationPage2 {
 	
 	WebDriver driver;
 	
+	@FindBy(xpath = "/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr"
+			+ "/td[2]/table/tbody/tr[5]/td/form/table[1]/tbody/tr[3]/td[1]/input")
+	WebElement depBlue360;
 	
+	@FindBy(xpath = "/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr"
+			+ "/td[2]/table/tbody/tr[5]/td/form/table[1]/tbody/tr[7]/td[1]/input")
+	WebElement depPang362;
+	
+	@FindBy(xpath = "/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr"
+			+ "/td[2]/table/tbody/tr[5]/td/form/table[2]/tbody/tr[3]/td[1]/input")
+	WebElement retBlue630;
+	
+	@FindBy(xpath = "/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr"
+			+ "/td[2]/table/tbody/tr[5]/td/form/table[2]/tbody/tr[7]/td[1]/input")
+	WebElement retPang632;
 	
 	public ReservationPage2() {
 		driver = Init.getDriver();
 		PageFactory.initElements(driver, this);
 	}
 
+	public void depBlue360Clicker() {
+		depBlue360.click();
+	}
+	
+	public void depPang362Clicker() {
+		depPang362.click();
+	}
+	
+	public void retBlue630Clicker() {
+		retBlue630.click();
+	}
+	
+	public void retPang632Clicker() {
+		retPang632.click();
+	}
+	
+	public void bothClicker() {
+		depPang362.click();
+		retPang632.click();
+	}
 }
