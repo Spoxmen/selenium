1afe633747d363ece19c15a33b6f4a5ac7d13b45
Spoxmen <spoxmen@gmail.com>
2019-06-21 19:06:21 +0200

    added working cucumber class

---
+++ src/test/java/pl/lait/Steps/LoginPageStepsDefs.java
@@ -0,0 +1,40 @@
+package pl.lait.Steps;
+
+import org.openqa.selenium.WebDriver;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import pageObjects.LoginPage;
+import pageObjects.MainPage;
+import pageObjects.ReservationPage;
+import pl.lait.selenium.Init;
+
+public class LoginPageStepsDefs {
+	
+	WebDriver driver = Init.getDriver();
+	MainPage mp = new MainPage();
+	LoginPage lp = new LoginPage();
+	ReservationPage rp = new ReservationPage();
+	
+	@Given("^I open main page$")
+	public void i_open_main_page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println(driver.getTitle());
+	}
+
+	@When("^I click SIGN-ON link$")
+	public void i_click_SIGN_ON_link() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		mp.signonLinkClick();
+	}
+
+	@Then("^I should see login page$")
+	public void i_should_see_login_page() throws Throwable {
+		System.out.println(driver.getTitle());
+		
+		
+	}
+	
+}
